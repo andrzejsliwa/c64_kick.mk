393ceb70f87d03e6b24b79f57c4cf5327891056a
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2015-07-14 21:41:38 +0200

    cleanup

--- src/example.asm
+++ src/example.asm
@@ -1,10 +1,10 @@
 .import source "memory_map.inc"
 .import source "tools.inc"
 
-.var GLOBAL_VIC_BANK_CONFIG    = 0 // 0 - default ($0000 - $3fff)
-.var GLOBAL_VIC_SCREEN_CONFIG  = 1 // 1 - default ($0400)
-.var GLOBAL_VIC_CHARSET_CONFIG = 2 // 2 - default ($1000 - $17ff)
-.var GLOBAL_CPU_PORT_MEMORY_CONFIG  = %101 // %111 - default
+.var GLOBAL_VIC_BANK_CONFIG        = 0    // 0 - default ($0000 - $3fff)
+.var GLOBAL_VIC_SCREEN_CONFIG      = 1    // 1 - default ($0400)
+.var GLOBAL_VIC_CHARSET_CONFIG     = 2    // 2 - default ($1000 - $17ff)
+.var GLOBAL_CPU_PORT_MEMORY_CONFIG = %101 // %111 - default
 
 .import source "global_configuration.inc"
 
@@ -16,17 +16,5 @@ start:
     :VIC_global_setup_screen_memory()
     :VIC_global_setup_charset_memory()
     :CPU_PORT_memory_setup()
-
-
-    
-//     ldx #0
-// loop:
-// 	lda $d000, x
-// 	sta GLOBAL_VIC_CHARSET_ADDRESS, x
-//     cpx #$ff
-//     beq next
-//     inx
-//     jmp loop
-// next:
     rts
 
